bebbc92cc2dc821d8b5974138bdc095bb135792d
rintarokojima <rin-tennis-0515@yahoo.ne.jp>
2022-10-29 11:48:18 +0900

    セッション5 バリデーション

--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -50,8 +50,8 @@
                                             <button type="button" onclick="location.href='{{route('admin.owners.store')}}'" class=" bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                                             <button type="submit" class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
                                         </div>
-                                 </div>
-                            </form>
+                                    </div>
+                                </form>
                         </div>
                     </section>
                 </div>
