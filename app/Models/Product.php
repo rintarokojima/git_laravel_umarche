f4ff4ee87da37da3c2d61fcc990e12788c45d419
rintarokojima <rin-tennis-0515@yahoo.ne.jp>
2022-12-14 17:25:00 +0900

    カート表示

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,7 @@ use App\Models\Shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
 use App\Models\Stock;
+use App\Models\User;
 
 
 
@@ -63,5 +64,11 @@ class Product extends Model
     public function stock()
     {
       return $this->hasMany(Stock::class);
+    }
+
+    public function users()
+    {
+      return $this->belongsToMany(Product::class, 'carts')
+        ->withPivot(['id', 'quantity']); 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
 
 /*
@@ -24,6 +25,15 @@ Route::middleware('auth:users')->group(function(){
         Route::get('/', [ItemController::class, 'index'])->name('items.index');
         Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
+
+Route::prefix('cart')->middleware('auth:users')->group(function(){
+        Route::get('/',[CartController::class,'index'])->name('cart.index');
+        Route::post('add', [CartController::class, 'add'])->name('cart.add');
+        Route::post('delete/{item}',[CartController::class,'delete'])->name('carte.delete');
+        
+        
+});
+
 
 Route::get('/dashboard', function () {
     return view('user.dashboard');
